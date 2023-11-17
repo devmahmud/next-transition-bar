5ed4ec5627756d19dff433f6b5fcf1248957844a
Mahmudul Alam <mahmudul@ccprotocol.xyz>
2023-11-17 23:00:49 +0600

    chore: pre-commit hook

---
+++ styleTypes.d.ts
@@ -0,0 +1,10 @@
+import 'react';
+
+declare module 'react' {
+  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    jsx?: string;
+    global?: string;
+    precedence?: string;
+    href?: string;
+  }
+}
